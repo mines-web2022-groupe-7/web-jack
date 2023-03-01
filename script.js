628e12e2d8eb5dff9976cd701f069904a68ef5e5
Raphael P <raphael.prasquier@imagine-app.fr>
2023-03-01 09:47:16 +0100

    Improvements

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
 
-let deckApiUrl = 'https://deckofcardsapi.com/api/deck'
-let drawButton = document.querySelector('#draw-cards');
-let playerCards = document.querySelector('#player-cards');
 
+let buttons = document.querySelectorAll('.btn')
 
-drawButton.addEventListener('click', function(e) {
+buttons.forEach(btn => btn.addEventListener('click', function(e) {
   e.preventDefault()
 
-  fetch(deckApiUrl + "/new/draw/?count=2")
-    .then(response => response.json())
-    .then(data => data.cards)
-    .then(cards => {
-      console.log(cards)
-      cards.forEach(card => {
-        let container = document.createElement("div")
-        container.classList.add("card")
-        container.append(document.createTextNode(card.value + " of " + card.suit))
+  alert('clicked')
 
-        let imageEl = document.createElement("img")
-        imageEl.src = card.image
-        container.append(imageEl)
-
-        playerCards.append(container)
-      })
-    })
-})
+}))
 
 document.addEventListener('DOMContentLoaded', function() {
 
